88f3a740c720b03bcad64f595217a2cc63c608d5
Aly Matrawy <alymatrawy@gmail.com>
2024-03-02 20:58:05 -0500

    started db.js

---
+++ database/db.js
@@ -0,0 +1,23 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const credentials = '<path_to_certificate>'
+
+const client = new MongoClient('mongodb+srv://studycircuit.wmxqxd5.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&appName=studycircuit', {
+  tlsCertificateKeyFile: credentials,
+  serverApi: ServerApiVersion.v1
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("testDB");
+    const collection = database.collection("testCol");
+    const docCount = await collection.countDocuments({});
+    console.log(docCount);
+    // perform actions using client
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
